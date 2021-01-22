2f74cf29e420340e590857091fdedc73a5097971
Pooja Hegde <poojabh08@gmail.com>
2021-01-22 09:20:38 +0530

    STLab

--- quicksort.py
+++ quicksort.py
@@ -1,32 +1,20 @@
-
-def partition(a, s, h):
-    pi = a[s]
-    l = s + 1
-    h = h
-
-    while True:
-        
-        while l <= h and a[h] >= pi:
-            h = h - 1
-
-        while l <= h and a[l] <= pi:
-            l = l + 1
-
-        if l <= h:
-            a[l], a[h] = a[h], a[l]
-        else:
-            break
-
-    a[s], a[h] = a[h], a[s]
-
-    return h
-
-
-
-def quicksort(a,l,h):
-    if(l<h):
-        p=partition(a,l,h)
-        quicksort(a,l,p-1)
+def quicksort(a,l1,h):
+    if(l1<h):
+        pi = a[l1]
+        l = l1 + 1
+        h = h
+        while True:
+            while l <= h and a[h] >= pi:
+                h = h - 1
+            while l <= h and a[l] <= pi:
+                l = l + 1
+            if l <= h:
+                a[l], a[h] = a[h], a[l]
+            else:
+                break
+        a[l1], a[h] = a[h], a[l1]
+        p = h
+        quicksort(a,l1,p-1)
         quicksort(a,p+1,h)
 
 n=int(input("Enter no of elements: "))
